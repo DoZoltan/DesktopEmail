fff2a702dcacaf757eabe0462a1af2d51370a03d
veslorand <61459386+veslorand@users.noreply.github.com>
2021-01-20 00:17:20 +0100

    Add CanLogin

---
+++ SaintSender.DesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                SetProperty(ref _email, value);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public bool CanLogIn(string email, string password)
+        {
+            bool output = false;
+            if (email.Length > 0 && password.Length > 0)
+            {
+                output = true;
+            }
+            return output;
+        }
+    }
+}
