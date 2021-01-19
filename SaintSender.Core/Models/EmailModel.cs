334ce15eea2d1c89a7bbf50c0c91ffa912d729ac
veslorand <61459386+veslorand@users.noreply.github.com>
2021-01-20 00:27:42 +0100

    Add EmailModel with 5 prop

---
+++ SaintSender.Core/Models/EmailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Entities
+{
+    public class EmailModel
+    {
+        public EmailModel(string sender, string subject, DateTime date, string read, string message)
+        {
+            Sender = sender;
+            Message = message;
+            Date = date;
+            Read = read;
+            Subject = subject;
+        }
+
+        public string Sender { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string Read { get; set; }
+        public string Subject { get; set; }
+    }
+}
