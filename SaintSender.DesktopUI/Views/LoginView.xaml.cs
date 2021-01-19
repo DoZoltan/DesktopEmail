3534f9945c9c2fd8a5f262dd1e923bb99fa97783
veslorand <61459386+veslorand@users.noreply.github.com>
2021-01-19 22:04:31 +0100

    Login form created

---
+++ SaintSender.DesktopUI/Views/LoginView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaintSender.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsValidEmail(EmailTxt.Text) || PasswordTxt.Password.Length < 4)
+            {
+                WrongEmailOrPW.Visibility = Visibility.Visible;
+            }
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mail = new System.Net.Mail.MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
